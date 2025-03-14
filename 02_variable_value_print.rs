5b55f46d14179ed438aa5b4dcd0bf15f55e5eca4
Surya-Manivannan <suryagyani@gmail.com>
2025-03-14 11:46:42 +0530

    Declare variable and print its Value

---
+++ 02_variable_value_print.rs
@@ -0,0 +1,26 @@
+dffn main() {
+//Integer
+let age: i32 = 25;
+println!("Age: {}", age);
+
+//String
+let name: String = String::from("John");
+println!("Name: {}", name);
+
+//Float
+let percentage: float64 = 7.75;
+println!("Percentage: {}", percentage);
+
+//Boolean
+let result: bool = true;
+println!("Result: {}", result);
+
+//Character
+let grade: char = 'A';
+println!("Grade: {}", grade);
+
+//Static string slice
+let greeting: &str = "Very Good";
+println!("Greeting: {}", greeting);
+
+}
